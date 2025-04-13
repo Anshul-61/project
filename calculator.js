3203b910e098443762b235867c3c25c4c8784202
GauranshNayal <beastron01@gmail.com>
2025-04-13 22:32:50 +0530

    comments

--- calculator.js
+++ calculator.js
@@ -1,21 +1,21 @@
+//storing values in variables
 const weightInput = document.getElementById('weight');
 const heightInput = document.getElementById('height');
 const calculateButton = document.getElementById('calculate-bmi-btn');
 const resultDiv = document.getElementById('result');
-
+//calculation
 function calculateBMI() {
   const weight = parseFloat(weightInput.value);
-  const height = parseFloat(heightInput.value) / 100; // Convert height to meters
-  
+  const height = parseFloat(heightInput.value) / 100; 
+//condition checker 
   if (isNaN(weight) || isNaN(height) || weight <= 0 || height <= 0) {
     resultDiv.textContent = 'Please enter valid weight and height.';
     resultDiv.classList.remove('underweight', 'normal', 'overweight', 'obese');
     return;
   }
-
   const bmi = weight / (height * height);
   let bmiCategory = '';
-
+//bmi result
   if (bmi < 18.5) {
     bmiCategory = 'underweight';
   } else if (bmi >= 18.5 && bmi < 24.9) {
@@ -25,7 +25,7 @@ function calculateBMI() {
   } else {
     bmiCategory = 'obese';
   }
-
+  //display
   resultDiv.textContent = `Your BMI is ${bmi.toFixed(2)}.`;
   resultDiv.classList.remove('underweight', 'normal', 'overweight', 'obese');
   resultDiv.classList.add(bmiCategory);
